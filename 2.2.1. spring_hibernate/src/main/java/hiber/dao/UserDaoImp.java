7128fcafb364c18ec49c880541caaa07ca9dadcd
ArtemErshov <109722078+ArtemErshof@users.noreply.github.com>
2022-12-28 18:01:23 +0300

    task_2.2.1

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -35,8 +35,7 @@ public class UserDaoImp implements UserDao {
 
     @Override
     public User getUserByModelAndSeries(String model, int series) {
-        String query = "select u from User u where u.car.model = :model and u.car.series = :series";
-        return sessionFactory.getCurrentSession().createQuery(query, User.class)
+        return sessionFactory.getCurrentSession().createQuery("select u from User u where u.car.model = :model and u.car.series = :series", User.class)
                 .setParameter("model", model)
                 .setParameter("series", series)
                 .getSingleResult();
